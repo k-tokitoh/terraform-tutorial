a0ad57aef4abeacf31810ed41ecf83e393e5cd34
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-24 20:52:25 +0900

    backend

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ terraform {
       version = ">= 3.0"
     }
   }
+
+  # 複数人で開発する際に、集権的にtfstateが同期される必要があるので、S3にtfstateを保存する
+  # backendはtfstateの保存場所を意味する
+  backend "s3" {
+    bucket = "terraform-tutorial-tfstate-bucket"
+    # オブジェクトのkey
+    key     = "terraform-tutorial.tfstate"
+    region  = "us-east-1"
+    profile = "private"
+  }
 }
 
 provider "aws" {
