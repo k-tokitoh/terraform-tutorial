18a88af9abf79a1758e78df6ce66001465649d77
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-24 20:37:24 +0900

    ec2 instance

--- appserver.tf
+++ appserver.tf
@@ -14,3 +14,25 @@ resource "aws_key_pair" "key_pair" {
     Environment = var.environment
   }
 }
+
+
+# ==========================================================================================================================
+# ec2 instance
+# ==========================================================================================================================
+
+resource "aws_instance" "application_server" {
+  ami                         = data.aws_ami.application_ami.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.application_security_group.id]
+
+  key_name = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-applicationServer"
+    project     = var.project
+    Environment = var.environment
+    Type        = "application"
+  }
+}
