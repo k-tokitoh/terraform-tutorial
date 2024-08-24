ff918d686d8ce7e70557e1abd5240986e21c06f6
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-24 17:33:51 +0900

    rds instance

--- rds.tf
+++ rds.tf
@@ -66,3 +66,67 @@ resource "random_string" "rds_password" {
   special = true
 }
 
+resource "aws_db_instance" "mysql_instance" {
+  engine         = "mysql"
+  engine_version = "8.0"
+
+  identifier = "${lower(var.project)}-${lower(var.environment)}-mysqlinstance"
+
+  username = "name"
+
+  # random stringはtfstateに平文で保存されてしまう
+  # - 対策1
+  #   - terraformを実行する人を、パスワードを知っていていい人に限定する
+  # - 対策2
+  #   - terraformを実行する人を、パスワードを知っていていい人に限定しない
+  #   - terraformの実行後に、terraform外から（aws cliなどにより）パスワードを変更する
+  #   - terraform上ではlifecycleの指定によって、terraformで管理されたstateと異なることが許容されるようにする
+  password = random_string.rds_password.result
+
+  instance_class = "db.t3.micro"
+
+  # 単位はGiB
+  allocated_storage     = 20
+  max_allocated_storage = 50
+
+  # gp2はデフォルトの選択肢で、汎用SSDを表す
+  storage_type = "gp2"
+
+  # ある時期からデフォルトで暗号化されるようになっている
+  storage_encrypted = true
+
+  # 今回はシングルAZで構築する
+  multi_az = false
+  # シングルAZなので、配置するAZを明示的に指定する必要がある
+  availability_zone    = "us-east-1a"
+  db_subnet_group_name = aws_db_subnet_group.mysql_subnet_group.name
+
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  publicly_accessible    = false
+  port                   = 3306
+  db_name                = "terraformTutorial"
+  parameter_group_name   = aws_db_parameter_group.mysql_paramter_group.name
+  option_group_name      = aws_db_option_group.mysql_option_group.name
+
+  # バックアップを実行する時間帯
+  backup_window = "03:00-04:00"
+  # バックアップの保持期間。単位は日。
+  backup_retention_period = 7
+
+  # 安全のため、バックアップの直後にメンテナンスウィンドウを設定する
+  maintenance_window         = "Mon:04:00-Mon:05:00"
+  auto_minor_version_upgrade = false
+
+  # 個人練習用なので、頻繁に削除をする見込み。
+  deletion_protection = false
+  # 頻繁に削除をする見込みなので、snapshotが溜まっていくことを防ぐため、最終スナップショットはとらない設定とする
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysqlInstance"
+    Project = var.project
+    Env     = var.environment
+  }
+}
