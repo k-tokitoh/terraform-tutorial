bcb5a7c8794a4f9461ac8f3689f8f3f91e3670a0
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-24 16:13:56 +0900

    application security group

--- security_group.tf
+++ security_group.tf
@@ -72,6 +72,46 @@ resource "aws_security_group" "application_security_group" {
   }
 }
 
+resource "aws_security_group_rule" "application_in_tcp3000" {
+  security_group_id = aws_security_group.application_security_group.id
+
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.web_security_group.id
+}
+
+resource "aws_security_group_rule" "application_out_http" {
+  security_group_id = aws_security_group.application_security_group.id
+
+  type            = "egress"
+  protocol        = "tcp"
+  from_port       = 80
+  to_port         = 80
+  prefix_list_ids = [data.aws_prefix_list.s3_prefix_list.id]
+}
+
+resource "aws_security_group_rule" "application_out_https" {
+  security_group_id = aws_security_group.application_security_group.id
+
+  type            = "egress"
+  protocol        = "tcp"
+  from_port       = 443
+  to_port         = 443
+  prefix_list_ids = [data.aws_prefix_list.s3_prefix_list.id]
+}
+
+resource "aws_security_group_rule" "application_out_tcp3306" {
+  security_group_id = aws_security_group.application_security_group.id
+
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db_security_group.id
+}
+
 
 # ==========================================================================================================================
 # db
