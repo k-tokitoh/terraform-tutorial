5e238a9215f70d1cef60eb30ff59dde1336aaad1
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-25 16:24:44 +0900

    s3 public bucket

---
+++ s3.tf
@@ -0,0 +1,55 @@
+resource "random_string" "s3_unique_key" {
+  length = 6
+  # 特殊文字
+  special = false
+  upper   = false
+  lower   = true
+  numeric = false
+}
+
+# ==========================================================================================================================
+# static bucket
+# ==========================================================================================================================
+
+
+resource "aws_s3_bucket" "s3_static_bucket" {
+  # バケット名
+  bucket = "${lower(var.project)}-${lower(var.environment)}-${random_string.s3_unique_key.result}"
+
+}
+
+# 
+resource "aws_s3_bucket_public_access_block" "s3_static_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_static_bucket.bucket
+
+  # 「パブリックアクセスOKだよ」というACLの追加をブロックする
+  block_public_acls = true
+
+  # 「パブリックアクセスOKだよ」というACLが元から存在していた場合、その許可を無視する（パブリックアクセスを禁じる）
+  ignore_public_acls = true
+
+  # 「パブリックアクセスOKだよ」というpolicyの追加をブロックする
+  block_public_policy = false
+
+  # 「パブリックアクセスOKだよ」というpolicyが元から存在していた場合、その許可を無視する（パブリックアクセスを禁じる）
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "s3_static_bucket_policy" {
+  bucket = aws_s3_bucket.s3_static_bucket.bucket
+  policy = data.aws_iam_policy_document.s3_static_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "s3_static_bucket_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.s3_static_bucket.bucket}/*"]
+    principals {
+      # ユーザーでも、AWSリソースでも
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+  }
+}
