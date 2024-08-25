fc07d27bb043c67f3c62009285bb4a64183cb73e
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-25 16:34:16 +0900

    s3 private bucket

--- s3.tf
+++ s3.tf
@@ -11,14 +11,13 @@ resource "random_string" "s3_unique_key" {
 # static bucket
 # ==========================================================================================================================
 
-
+# 配信したい静的 = staticなファイルを配置する、publicなバケット
 resource "aws_s3_bucket" "s3_static_bucket" {
   # バケット名
-  bucket = "${lower(var.project)}-${lower(var.environment)}-${random_string.s3_unique_key.result}"
+  bucket = "${lower(var.project)}-${lower(var.environment)}-static-${random_string.s3_unique_key.result}"
 
 }
 
-# 
 resource "aws_s3_bucket_public_access_block" "s3_static_bucket_public_access_block" {
   bucket = aws_s3_bucket.s3_static_bucket.bucket
 
@@ -51,5 +50,51 @@ data "aws_iam_policy_document" "s3_static_bucket_policy" {
       identifiers = ["*"]
     }
 
+  }
+}
+
+
+# ==========================================================================================================================
+# deploy bucket
+# ==========================================================================================================================
+
+# auto scalingにおいて必要なアプリケーションのソースコードを配置するための、privateなバケット
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  # バケット名
+  bucket = "${lower(var.project)}-${lower(var.environment)}-deploy-${random_string.s3_unique_key.result}"
+
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.bucket
+
+  # 「パブリックアクセスOKだよ」というACLの追加をブロックする
+  block_public_acls = true
+
+  # 「パブリックアクセスOKだよ」というACLが元から存在していた場合、その許可を無視する（パブリックアクセスを禁じる）
+  ignore_public_acls = true
+
+  # 「パブリックアクセスOKだよ」というpolicyの追加をブロックする
+  block_public_policy = true
+
+  # 「パブリックアクセスOKだよ」というpolicyが元から存在していた場合、その許可を無視する（パブリックアクセスを禁じる）
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket_policy" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.bucket
+  policy = data.aws_iam_policy_document.s3_deploy_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.s3_deploy_bucket.bucket}/*"]
+    principals {
+      # application serverを展開するec2インスタンスに付与するロール（これはinstance profileでない点に注意）
+      type        = "AWS"
+      identifiers = [aws_iam_role.application_server_iam_role.arn]
+    }
   }
 }
